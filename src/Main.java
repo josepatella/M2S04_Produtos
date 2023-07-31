014f80d383af36987d099f81099faf1e01c5068c
Patella <jfpatella@gmail.com>
2023-07-31 14:18:10 -0300

    No método main() criada uma Lista para armazenar os produtos.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import entidades.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Produtos!");
+       List<String> produtos = new ArrayList<>();
     }
 }
